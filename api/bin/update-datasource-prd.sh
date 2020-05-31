57efe3001ca7a1f3cc5775731042890a8bbde39a
00f100 <joaomoraesbr@gmail.com>
2020-05-31 08:56:33 -0300

    fix limit of countries to load

--- api/bin/update-datasource-prd.sh
+++ api/bin/update-datasource-prd.sh
@@ -2,7 +2,7 @@
 rm -Rf /var/jenkins/extractor 
 git clone https://github.com/00F100/covid-chart.git /var/jenkins/extractor
 cp /var/jenkins/extractor/api/.env-prd-extractor /var/jenkins/extractor/api/.env
-cd /var/jenkins/extractor/api && npm install && npm run build && sh bin/updateall.sh 25
+cd /var/jenkins/extractor/api && npm install && npm run build && sh bin/updateall.sh 50
 cp /var/jenkins/extractor/api/infra/data/database.sqlite /var/www/database.sqlite
 sudo kill `ps -ef | grep node | grep -v grep | awk '{print $2}'` || true
 cd /var/www/api && sudo nohup node dist/bootstrap/web.js > /var/log/covidlast-api-nohup.log &
